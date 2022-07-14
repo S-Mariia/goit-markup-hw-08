cd513f3eb644a2cd967b6637ee25921f922e39a7
Mariia Savchyn <savchyn.mar@gmail.com>
2022-07-14 12:54:09 +0300

    hw-second-version

---
+++ js/menu.js
@@ -0,0 +1,18 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.mob-header__open-btn'),
+    closeMenuBtn: document.querySelector('.mob-header__close-btn'),
+    visibleMenu: document.querySelector('.mob-header__invisible-part'),
+    invisibleMenu: document.querySelector('.mob-header__visible-part'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.visibleMenu.classList.toggle('is-hidden');
+    refs.invisibleMenu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
